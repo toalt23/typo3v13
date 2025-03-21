7fa9f64a3fecd7a7a7e53a8694ad722c36085d56
Tobias <to.alt@web.de>
2025-03-21 14:07:55 +0100

    Extbase

--- packages/sitepackage/ext_localconf.php
+++ packages/sitepackage/ext_localconf.php
@@ -13,8 +13,8 @@ ExtensionUtility::configurePlugin(
     // arbitrary, but unique plugin name (not visible in the backend)
     'PersonPlugin',
     // all actions
-    [PersonController::class => 'index'],
+    [PersonController::class => 'index',PersonController::class => 'new'],
     // non-cacheable actions
-    [PersonController::class => 'index'],
+    [PersonController::class => 'index', PersonController::class => 'new'],
     ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
 );
