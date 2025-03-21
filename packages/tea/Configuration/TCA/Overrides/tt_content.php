c6590478f293b2e215fdae1f620ac6f87bcd3058
Tobias <to.alt@web.de>
2025-03-21 17:11:33 +0100

    Tea Extension

--- packages/tea/Configuration/TCA/Overrides/tt_content.php
+++ packages/tea/Configuration/TCA/Overrides/tt_content.php
@@ -15,21 +15,21 @@ call_user_func(
             // plugin title, as visible in the drop-down in the BE
             'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_index',
             // the icon visible in the drop-down in the BE
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
+            'EXT:tea/Resources/Public/Icons/tea_icon.svg'
         );
 
         ExtensionUtility::registerPlugin(
             'Tea',
             'TeaShow',
             'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_show',
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
+            'EXT:tea/Resources/Public/Icons/tea_icon.svg'
         );
 
         ExtensionUtility::registerPlugin(
             'Tea',
             'TeaFrontEndEditor',
             'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_frontend_editor',
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
+            'EXT:tea/Resources/Public/Icons/tea_icon.svg'
         );
 
         // This removes the default controls from the plugin.
